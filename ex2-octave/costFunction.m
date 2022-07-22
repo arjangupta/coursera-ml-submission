47470f9a10098b6fc08e595e3042242347499a0e
Arjan Gupta <arjangupta95@gmail.com>
2022-07-21 20:13:44 -0500

    Write first stab at cost fn

--- ex2-octave/costFunction.m
+++ ex2-octave/costFunction.m
@@ -20,12 +20,9 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+h = sigmoid(theta'*X);
 
-
-
-
-
-
+J = (1/m)*(-y*log(h)-(1-y)*log(1-h));
 
 % =============================================================
 
