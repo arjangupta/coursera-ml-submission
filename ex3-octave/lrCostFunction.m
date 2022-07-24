0e06935426810210423fb23944e82ef38462cdae
Arjan Gupta <arjangupta95@gmail.com>
2022-07-24 18:11:16 -0500

    Fix bug in J (don't include theta0), write vectorized gradient

--- ex3-octave/lrCostFunction.m
+++ ex3-octave/lrCostFunction.m
@@ -38,7 +38,9 @@ grad = zeros(size(theta));
 
 h = sigmoid(X*theta);
 
-J = (1/m)*( -y'*log(h)-(1-y')*log(1-h) ) + (lambda/(2*m))*(theta'*theta);
+J = (1/m)*( -y'*log(h)-(1-y')*log(1-h) ) + (lambda/(2*m))*( theta(2:end)' * theta(2:end) );
+
+grad = (1/m) * ( X' * (h-y) ) + [0; ((lambda/m) * theta(2:end))];
 
 % =============================================================
 
