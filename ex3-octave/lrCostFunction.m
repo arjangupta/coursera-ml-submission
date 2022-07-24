ce979ee8c6da2531cb906a6e6f2fc476dc4f0e61
Arjan Gupta <arjangupta95@gmail.com>
2022-07-24 17:48:20 -0500

    Write first stab at vectorized cost fn

--- ex3-octave/lrCostFunction.m
+++ ex3-octave/lrCostFunction.m
@@ -36,14 +36,9 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+h = sigmoid(X*theta);
 
-
-
-
-
-
-
-
+J = (1/m)*( -y'*log(h)-(1-y')*log(1-h) ) + (lambda/(2*m))*(theta'*theta);
 
 % =============================================================
 
