1044946b43f26d366ad02983d4338faa714175c3
Arjan Gupta <arjan.gupta@lindsay.com>
2022-08-10 16:54:09 -0500

    Fix the issue in ranInitWeights

--- ex4-octave/randInitializeWeights.m
+++ ex4-octave/randInitializeWeights.m
@@ -20,7 +20,7 @@ W = zeros(L_out, 1 + L_in);
 %
 
 epsiloninit = 0.12;
-W = rand(Lout, 1 + Lin) * (2 * epsiloninit) - epsiloninit;
+W = rand(L_out, 1 + L_in) * (2 * epsiloninit) - epsiloninit;
 
 
 
