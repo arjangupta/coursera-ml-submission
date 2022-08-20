0d9175408571d1ddbe071a126bb4609b0c707331
Arjan Gupta <arjangupta95@gmail.com>
2022-08-20 00:19:55 -0500

    Implement an algorithm to choose best C and sigma

--- ex6-octave/dataset3Params.m
+++ ex6-octave/dataset3Params.m
@@ -23,11 +23,56 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+C = 0.01;
+sigma = 0.01;
 
+% Train and evaluate C and sigma with these initial values
+model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+predictions = svmPredict(model, Xval);
+err = mean(double(predictions ~= yval));
 
+% Set this as the minimum error seen so far, and best C and sigma so far
+min_err = err;
+best_C = C;
+best_sigma = sigma;
 
+base_multiplicand_C = C;
+base_multiplicand_sigma = sigma;
 
+for i = 1:8
+    % Change C
+    if (rem(i/2) == 0)
+        base_multiplicand_C *= 10;
+        C = base_multiplicand_C;
+    else
+        C = 3 * base_multiplicand_C;
+    endif
 
+    for j = 1:8
+        % Change sigma
+        if (rem(j/2) == 0)
+            base_multiplicand_sigma *= 10;
+            sigma = base_multiplicand_sigma;
+        else
+            sigma = 3 * base_multiplicand_sigma;
+        endif
+
+        % Train and evaluate C and sigma
+        model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+        predictions = svmPredict(model, Xval);
+        err = mean(double(predictions ~= yval));
+
+        % If error is lowest ever, then record this
+        if (err < min_err)
+            min_err = err;
+            best_C = C;
+            best_sigma = sigma;
+        endif
+    end
+end
+
+C = best_C;
+sigma = best_sigma;
 
 % =========================================================================
 
